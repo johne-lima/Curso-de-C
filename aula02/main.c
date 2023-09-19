0de9ab216460074a2e147ee5b2eb23f652dcfb4e
Johne Lima <johne.keyvid0@gmail.com>
2023-09-19 14:28:11 -0300

    aula02

---
+++ aula02/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//caractere de escape para saltar uma linha \n
+int main()
+{
+    //printf é uma função de saída
+    printf("\nOlá. \nSeja \nbem \nvindo!\n");
+
+    printf("\n---------------------------\n");
+    printf("\n1 - Logar  2 - Cadastrar  3 - Imprimir\n");
+    printf("\n---------------------------\n");
+
+    printf("Valor retornado: %d", printf("\nOi"));
+    return 0;
+}
