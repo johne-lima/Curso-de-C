409955bbf65fcccfff1835c81d667cce7681486a
Johne Lima <johne.keyvid0@gmail.com>
2023-09-19 14:13:53 -0300

    aula01

---
+++ aula01/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+        BLOCO DE 
+        COMENTÁRIO
+    */
+    
+    printf("Aprendendo comentários na linguagem C \n");
+
+    //printf("Hello world!\n")
+    return 0;
+}
